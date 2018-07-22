1b8ee07603cd82eeee3377666de8fee6dd365135
dogusumit <dogusumit@yandex.com>
2018-07-22 22:49:20 +0300

    Add files via upload

---
+++ app/src/main/java/com/dogusumit/calendar/MainActivity.java
@@ -0,0 +1,99 @@
+package com.dogusumit.calendar;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatePicker datePicker = findViewById(R.id.datePicker);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        if(Build.VERSION.SDK_INT >= 21){
+            calendarView.setVisibility(View.GONE);
+            datePicker.setSpinnersShown(false);
+        }
+        else {
+            datePicker.setVisibility(View.GONE);
+        }
+    }
+
+    private void toastla(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    private void uygulamayiOyla() {
+        Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
+            } catch (Exception ane) {
+                toastla(e.getMessage());
+            }
+        }
+    }
+    private void marketiAc() {
+        try {
+            Uri uri = Uri.parse("market://developer?id=" + getString(R.string.play_store_id));
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/developer?id=" + getString(R.string.play_store_id))));
+            } catch (Exception ane) {
+                toastla(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.oyla:
+                uygulamayiOyla();
+                return true;
+            case R.id.market:
+                marketiAc();
+                return true;
+            case R.id.cikis:
+                System.exit(0);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
